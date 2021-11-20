7d6aec7703e64dab1e0c23caeb97b1f13006904c
Ryan Tucker <nick.tucker.bass@gmail.com>
2021-11-20 02:05:37 -0500

    header load safari compromise

--- js/headerLoad.js
+++ js/headerLoad.js
@@ -2,29 +2,35 @@ const [main, menu1, menu2] = [...document.querySelectorAll("canvas")];
 menu1.style.display = "none";
 menu2.style.display = "none";
 //for typing effect
-let letters;
-const nameContainer = document.querySelector("#name-container");
-const spans = "Ryan Tucker".split("").map((letter) => {
-  if (!letter) return `<span class='initial'>&nbsp;</span>`;
-  return `<p class='main-name-letter name initial'>${letter}</p>`;
-});
-for (let i = 0; i < spans.length; i++) {
-  setTimeout(() => {
-    nameContainer.innerHTML += spans[i];
-  }, 100 * i);
-}
-setTimeout(() => {
-  document.querySelectorAll(".main-name-letter").forEach((letter) => {
-    nameContainer.classList.remove("initial");
-    nameContainer.classList.add("loaded");
-    letter.addEventListener("mouseenter", () => {
-      // letter.classList.add("snap", "animated-longer");
-      console.dir(letter)
-      letter.style.color='red'
-      letter.style.textShadow = 'none'
-    });
-    letter.addEventListener("mouseout", () => {
-      letter.classList.remove("snap", "animated-longer");
-    });
+function animatedHeader() {
+  const nameContainer = document.querySelector("#name-container");
+  const spans = "Ryan Tucker".split("").map((letter) => {
+    if (!letter) return `<span class='initial'>&nbsp;</span>`;
+    return `<p class='main-name-letter name initial'>${letter}</p>`;
   });
-}, 1150);
+  for (let i = 0; i < spans.length; i++) {
+    setTimeout(() => {
+      nameContainer.innerHTML += spans[i];
+    }, 100 * i);
+  }
+  setTimeout(() => {
+    const letters = [...document.querySelectorAll(".main-name-letter")];
+    letters.forEach((letter) => {
+      nameContainer.classList.remove("initial");
+      nameContainer.classList.add("loaded");
+      letter.addEventListener("mouseenter", (e) => {
+        letter.classList.remove("initial");
+        letter.style.textShadow = "none";
+        if (
+          letters.every((letter) => ![...letter.classList].includes("initial"))
+        ) {
+          setTimeout(() => {
+            letters.forEach((letter) => nameContainer.removeChild(letter));
+            animatedHeader();
+          }, 1000);
+        }
+      });
+    });
+  }, 1150);
+}
+animatedHeader();
