326b572b696139f4a3483cc34fc874062ac77c4d
Ryan Tucker <nick.tucker.bass@gmail.com>
2021-11-19 20:37:13 -0500

    safari check

--- js/headerLoad.js
+++ js/headerLoad.js
@@ -5,8 +5,8 @@ menu2.style.display = "none";
 let letters;
 const nameContainer = document.querySelector("#name-container");
 const spans = "Ryan Tucker".split("").map((letter) => {
-  if (!letter) return `<span>&nbsp;</span>`;
-  return `<p class='main-name-letter name'>${letter}</p>`;
+  if (!letter) return `<span class='initial'>&nbsp;</span>`;
+  return `<p class='main-name-letter name initial'>${letter}</p>`;
 });
 for (let i = 0; i < spans.length; i++) {
   setTimeout(() => {
@@ -14,6 +14,11 @@ for (let i = 0; i < spans.length; i++) {
   }, 100 * i);
 }
 setTimeout(() => {
-  nameContainer.classList.remove(["initial"]);
-  nameContainer.classList.add("loaded");
+  document.querySelectorAll('.main-name-letter').forEach((letter=>{
+    nameContainer.classList.remove(["initial"]);
+    nameContainer.classList.add("loaded");
+    letter.classList.remove(["initial"]);
+    
+    letter.classList.add("loaded");
+  }))
 }, 1150);
