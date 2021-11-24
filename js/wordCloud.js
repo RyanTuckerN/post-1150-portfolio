343cafba4b558132ff0db5eb3ccdea0e54f4fac4
Ryan Tucker <nick.tucker.bass@gmail.com>
2021-11-24 16:00:07 -0500

    updated css and js for word cloud

--- js/wordCloud.js
+++ js/wordCloud.js
@@ -1,17 +1,18 @@
+const words = document.querySelectorAll("#tech p");
+const skills = [...document.querySelectorAll('#tech-mobile li span')]
+const header = document.querySelector('header#masthead')
+const about = document.querySelector('section#about')
+const techBubbles = document.querySelector('#tech')
 
 function randInt(min, max) {
   return Math.floor(Math.random() * ((max + 1) - min) ) + min;
 }
-
-const words = document.querySelectorAll("#tech p");
-const wordsContainer = document.querySelector('#tech')
 
 for (let i = 0; i < words.length; i++){
   let newX = randInt(0, 100);
   let newY = randInt(0, 100);
   
   let topValue = "calc("+ newY.toString() + "% - 1ex)";
-  // let leftValue = `calc(${newX.toString()}% - ${80}px )`;
   let leftValue = `calc(${newX.toString()}% - ${words[i].clientWidth}px )`;
   
   words[i].style.top = topValue;
@@ -28,14 +29,13 @@ setInterval(function updateWordsPositions(){
     let newY = randInt(0, 100);
     
     let topValue = "calc("+ newY.toString() + "% - 1ex)";
-    let leftValue = newX.toString() + "%";
+    let leftValue = `calc(${newX.toString()}%`;
     
     words[i].style.top = topValue;
     words[i].style.left = leftValue;
   }
 }, 8000);
 
-const skills = [...document.querySelectorAll('#tech-mobile li span')]
 skills.forEach(skill=>{
   
   const skillBubble = document.querySelector(`[data-tech*="${skill.innerHTML}"]`)
@@ -49,10 +49,7 @@ skills.forEach(skill=>{
   })
   
 })
-const header = document.querySelector('header#masthead')
-const about = document.querySelector('section#about')
-const techBubbles = document.querySelector('#tech')
-techBubbles.style.top = `${about.offsetHeight + header.offsetHeight + 50}px`
+techBubbles.style.top = `${about.offsetHeight + header.offsetHeight + 110}px`
 window.addEventListener('resize', ()=>{
-techBubbles.style.top = `${about.offsetHeight + header.offsetHeight + 50}px`
+techBubbles.style.top = `${about.offsetHeight + header.offsetHeight + 110}px`
 })
